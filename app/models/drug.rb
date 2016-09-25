d0a7b9fe7b11daa9b392eb8acfb625753c38233d
Selvakumar <e1graje@MC02GCB8CDRJC.local>
2016-09-24 22:28:43 -0700

    tested

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -6,19 +6,15 @@ class Drug < ActiveRecord::Base
   
   # Scopes
   scope :alphabetical, order('name')
-  scope :prohibited, where('category = ?', 'prohibited')
-  scope :restricted, where('category = ?', 'restricted')
-  scope :permitted, where('category = ?', 'permitted')
+  scope :prohibited, where('category = ?', 'Following Medications are prohibited 30 days prior to screening and throughout the study period')
+  scope :restricted, where('category = ?', 'Following medications are allowed during the study; However, they are prohibited within 48 hours prior to taking IP')
+  scope :permitted, where('category = ?', 'Following Medications are allowed during the study; however they are prohibited within 24 hours prior to taking IP')
   scope :search, lambda { |term| where('medication_name LIKE ? OR brand_name LIKE ?', "#{term}%", "#{term}%").order("medication_name", "brand_name")  }
   scope :for_study, lambda {|study_id| where("study_id = ?", study_id) }
   
-  
-  
-  # Validations 
-  validates_inclusion_of :category, :in => %w[prohibited restricted permitted], :message => "is not an option"
-  
+
   # Misc
-  CATEGORIES_LIST = [['Prohibited', 'prohibited'],['Restricted', 'restricted'],['Permitted', 'permitted']]
+  CATEGORIES_LIST = [['Following Medications are prohibited 30 days prior to screening and throughout the study period', 'Following Medications are prohibited 30 days prior to screening and throughout the study period'],['Following medications are allowed during the study; However, they are prohibited within 48 hours prior to taking IP', 'Following medications are allowed during the study; However, they are prohibited within 48 hours prior to taking IP'],['Following Medications are allowed during the study; however they are prohibited within 24 hours prior to taking IP', 'Following Medications are allowed during the study; however they are prohibited within 24 hours prior to taking IP']]
   
   # Method
   
--- lib/tasks/populate.rake
+++ lib/tasks/populate.rake
@@ -26,29 +26,15 @@ namespace :db do
     
     alzheimers_medications1 = Drug.new
     alzheimers_medications1.medication_name = "Actos"
-    alzheimers_medications1.category = "restricted"
+    alzheimers_medications1.category = "Following Medications are allowed during the study; however they are prohibited within 24 hours prior to taking IP"
     alzheimers_medications1.study_id = study1.id
     alzheimers_medications1.save!
 
     alzheimers_medications = Drug.new
     alzheimers_medications.medication_name = "Alurate"
-    alzheimers_medications.category = "restricted"
+    alzheimers_medications.category = "Following Medications are allowed during the study; however they are prohibited within 24 hours prior to taking IP"
      alzheimers_medications.study_id = study1.id
     alzheimers_medications.save!
 
-    alzheimers_medications = Drug.new
-    alzheimers_medications.medication_name = "Alzheimers Medications"
-    alzheimers_medications.category = "restricted"
-    alzheimers_medications.information = "Allowed provided that the dose has been stable for 90 days prior to randomization.During double-blind treatment period Subject will remain on the same dose throughout the duration of the trial."
-    alzheimers_medications.study_id = study1.id
-    alzheimers_medications.save!
-
-    nmda_receptor_antagonist_medications = Drug.new
-    nmda_receptor_antagonist_medications.medication_name = "NMDA Receptor Antagonist Medications"
-    nmda_receptor_antagonist_medications.category = "restricted two"
-    nmda_receptor_antagonist_medications.information = "Allowed provided that the dose has been stable for 90 days prior to randomization.During double-blind treatment period Subject will remain on the same dose throughout the duration of the trial."
-    nmda_receptor_antagonist_medications.study_id = study1.id
-    nmda_receptor_antagonist_medications.save!
-       
    end
 end
